4bb86dc669e527d084374d0a4b3459b2d539b503
RomeoGolf <romeogolf@none.com>
2016-12-28 15:09:41 +0500

    Добавил InterfaceGuid лог. тома, закрыл условие выбора устройства

--- src/usb-polygon.cpp
+++ src/usb-polygon.cpp
@@ -36,7 +36,14 @@ int main()
     HDEVINFO hDevInfo;
     TCHAR *lpBuffer = NULL;
 
-    const GUID InterfaceGuid = { 0x53F56307,0xB6BF,0x11D0,{0x94,0xF2,0x00,0xA0,0xC9,0x1E,0xFB,0x8B} };
+    // \\?\usbstor#disk&ven_<name>&rev_0001#7&4e...
+    // GUID_DEVINTERFACE_DISK
+    //const GUID InterfaceGuid = { 0x53F56307,0xB6BF,0x11D0,{0x94,0xF2,0x00,0xA0,0xC9,0x1E,0xFB,0x8B} };
+
+    // \\?\storage#removablemedia#8...
+    // GUID_DEVINTERFACE_VOLUME
+    // нельзя FriendlyName
+    const GUID InterfaceGuid = { 0x53f5630dL, 0xb6bf, 0x11d0, {0x94, 0xf2, 0x00, 0xa0, 0xc9, 0x1e, 0xfb, 0x8b} };
 
     hDevInfo = SetupDiGetClassDevs( &InterfaceGuid,0, 0, DIGCF_PRESENT  | DIGCF_DEVICEINTERFACE  );
 
@@ -83,9 +90,8 @@ int main()
             }
         }
 
-        if( !_tcscmp(lpBuffer, _T("USBSTOR")) )
+        //if( !_tcscmp(lpBuffer, _T("USBSTOR")) )    // Точно USB накопитель...
         {
-            // Точно USB накопитель...
             _tprintf(_T("%s\n"), pDeviceInterfaceDetailData->DevicePath);
             /* \\?\usbstor#disk&ven_generic&prod_usb_flash_disk&rev_0.00#000000000000ec&0#{53f56307-b6bf-11d0-94f2-00a0c91efb8b} */
 
